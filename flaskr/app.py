0f3e9929adb48c643b9d58fb007408ee723051b7
Liangqu Chen <lchenbusiness@gmail.com>
2023-04-01 18:44:57 -0400

    push

--- flaskr/app.py
+++ flaskr/app.py
@@ -17,14 +17,16 @@ parks_df = uf.import_data(f"select * from wanderwisely.activity_related_parks", 
 
 
 # record user's selection
-user_selection = {"activities": [], "amenities": [], "parkName": [], "pois": []}
-
+user_selection = {"activities": [], "amenities": [], "pois": [],"hours":[], "park":[]}
 
 def update_selection(selection, select_type):
-    if selection in user_selection[select_type]:
-        user_selection[select_type].remove(selection)
-    else:
-        user_selection[select_type].append(selection)
+    if select_type == "hours" or select_type == "park":
+            user_selection[select_type] = [selection]
+    else: # when select_type == amenities, activities, pois
+        if selection in user_selection[select_type]:
+            user_selection[select_type].remove(selection)
+        else:
+            user_selection[select_type].append(selection)
 
 
 app = Flask(__name__)
@@ -40,13 +42,14 @@ def record_button():
     data = request.get_json()
     update_selection(data["input"], data["type"])
     # Record the button click in the database or perform any other action
-
+    print(user_selection)
     return '', 204
 
 @app.route('/parks')
 def parks():
-    top_three_parks = algo(user_selection)
-    return render_template('parks.html',parks = top_three_parks)
+    top_three_parks = ["Acadia National Park", "Arches National Park", "Capitol Reef National Park"]
+    hours = [1,2,3,4,5,6,7,8,9,10,11,12]
+    return render_template('parks.html',parks = top_three_parks, hours = hours)
 
 
 def generate_places(parkName, activities):
@@ -60,7 +63,7 @@ def generate_places(parkName, activities):
 
 @app.route('/poi')
 def poi():
-    parkName = user_selection['parkName']
+    parkName = user_selection['park'][0]
     activities = user_selection['activities']
     places = generate_places(parkName, activities)
     # parkName = 'Yosemite National Park'
